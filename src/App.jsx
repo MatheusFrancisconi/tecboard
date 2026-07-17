4a48eaf9ee59c37b684de559e21cf0d6bdd41271
MatheusF <mathesfrancisconi@gmail.com>
2026-07-17 17:35:41 -0400

    adicionando e estilizando componente Tema e Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
 import "./App.css";
+import { Banner } from "./components/Banner";
 import { FormularioDeEvento } from "./components/FormularioDeEventos/FormularioDeEventos";
+import { Tema } from "./components/Tema";
 
 //props é um objeto
 function App() {
+  const temas = [
+    { id: 1, nome: "front-end" },
+    { id: 2, nome: "back-end" },
+    { id: 3, nome: "devops" },
+    { id: 4, nome: "inteligencia artifical" },
+    { id: 5, nome: "data science" },
+    { id: 6, nome: "cloud" },
+  ];
+
   return (
     <main>
       <header>
         <img src="/logo.png" alt="" />
       </header>
-      <section>
-        <img src="/banner.png" alt="" />
-      </section>
+      <Banner />
       <FormularioDeEvento />
+      <section>
+        <Tema tema={temas[0]} />
+      </section>
+      <section>
+        <Tema tema={temas[1]} />
+      </section>
+      <section>
+        <Tema tema={temas[2]} />
+      </section>
+      <section>
+        <Tema tema={temas[3]} />
+      </section>
+      <section>
+        <Tema tema={temas[4]} />
+      </section>
+      <section>
+        <Tema tema={temas[5]} />
+      </section>
     </main>
   );
 }
